ae2f14a0606698a31c66e3dbf570fc7c9fc17418
waspwatch <73767163+waspwatch@users.noreply.github.com>
2020-11-24 09:52:13 +0900

    Update ImageScreen.js
    Test

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -6,7 +6,7 @@ const ImageScreen = () => {
     return <View>
         <ImageDetail title="Forest" imageSource={require('../../assets/forest.jpg')} score="9"/>
         <ImageDetail title="Beach" imageSource={require('../../assets/beach.jpg')} score="7"/>
-        <ImageDetail title="Mountain" imageSource={require('../../assets/mountain.jpg')} score="4"/>
+        <ImageDetail title="Mountain" imageSource={require('../../assets/mountain.jpg')} score="ScoreModified!"/>
     </View>
 };
 
