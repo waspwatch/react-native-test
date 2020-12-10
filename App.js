480f4b8881c1600835645aedf50e06c034aedaff
Kyle <Sinclaire@DESKTOP-PF7BGAB>
2020-12-10 13:37:35 +0000

    Empty react native project

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import ColorScreen from './src/screens/ColorScreen.js';
 
 const navigator = createStackNavigator({
-  Color: ColorScreen
+  //Color: ColorScreen
 }, {
+  /*
   initialRouteName: 'Color',
   defaultNavigationOptions: {
     title: 'State Test',
     headerTitleStyle: { alignSelf: 'center'}
   }
+  */
 });
 
 export default createAppContainer(navigator);
