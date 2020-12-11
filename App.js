5d8f8ba001c990f1e121fb5d837ae04ce4013fa4
EC2 Default User <ec2-user@ip-172-31-34-33.us-east-2.compute.internal>
2020-12-11 06:46:19 +0000

    Square Color

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import SquareScreen from './src/screens/SquareScreen.js'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const navigator = createStackNavigator({
+  Square: SquareScreen
+}, {
+  initialRouteName: 'Square',
+  defaultNavigationOptions: {
+    title: 'Color Square App',
+    headerTitleStyle: { alignSelf: 'center' }
+  }
 });
+
+export default createAppContainer(navigator);
