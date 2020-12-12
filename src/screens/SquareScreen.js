121566286fe60066c78e672e669a84a8a5b02543
Kyle <Sinclaire@DESKTOP-PF7BGAB>
2020-12-12 03:41:49 +0000

    Before using reducer community conventions

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -9,12 +9,11 @@ const reducer = (state, action) => {
     // action === { colorToChange: 'red' || 'green' || 'blue', amount : 15 || -15 }
     switch (action.colorToChange) {
         case 'red':
-            // never going to do: state.red = state.red - 15;
-            return { ...state, red: state.red + action.amount };
+            return state.red + action.amount > 255 || state.red + action.amount < 0 ? state : { ...state, red: state.red + action.amount };
         case 'green':
-            return { ...state, green: state.green + action.amount };
+            return state.green + action.amount > 255 || state.green + action.amount < 0 ? state : { ...state, green: state.green + action.amount };
         case 'blue':
-            return { ...state, blue: state.blue + action.amount };
+            return state.blue + action.amount > 255 || state.blue + action.amount < 0 ? state : { ...state, blue: state.blue + action.amount };
         default:
             return state;
     }
